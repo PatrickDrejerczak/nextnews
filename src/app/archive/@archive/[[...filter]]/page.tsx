a6a541ca6cf0748d539e18d9366893b3fb64c2ce
PatrickDrejerczak <drejerczak@aol.com>
2024-06-29 19:35:38 +0200

    added months to navigation

--- src/app/archive/@archive/[[...filter]]/page.tsx
+++ src/app/archive/@archive/[[...filter]]/page.tsx
@@ -1,29 +1,60 @@
 import Link from "next/link";
 import NewsList from "../../../../../components/news-list/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "../../../../../lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "../../../../../lib/news";
 
 export default function FilteredNewsPage({
   params,
 }: {
   params: { filter: string };
 }) {
-  const links = getAvailableNewsYears();
-  const newsYear = params.filter;
-  const filter = getNewsForYear(newsYear);
+  const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for this year.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
-              <li key={link}>
-                <Link href={`/archive/${link}`}>{link}</Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
-      <NewsList news={filter} />
+      {newsContent}
     </>
   );
 }
